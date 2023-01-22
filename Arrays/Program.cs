7253f25caa300f915e53c84be143da02a1232eb1
pvm <petrenkovm@hotmail.com>
2023-01-22 21:22:19 +0200

    add new functions: CountItems() and NewArray()

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] strArray1 = { "hello", "Vasiliy", "John", "Ann", "Go" };
+        string[] strArray2 = { "python", "c#", "gava", "c++", "R" };
+
+        int numSymbols = 3;
+
+        int CountItems(string[] srtArr)
+        {
+            int count = 0;
+
+            for (int i = 0; i < srtArr.Length; i++)
+            {
+                if (srtArr[i].Length <= numSymbols) count++;
+            }
+
+            return count;
+        }
+
+        string[] NewArray(string[] strArr)
+        {
+            int lenghtNewArr = CountItems(strArr);
+            string[] newArr = new string[lenghtNewArr];
+
+            foreach (string str in strArr)
+            {
+                int i = 0;
+                while (i < lenghtNewArr)
+                {
+                    if (str.Length <= numSymbols)
+                    {
+                        newArr[i] = str;
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine(newArr);
+            return newArr;
+        }
+
+        string[] newArray = NewArray(strArray1);
+    }
+}
+
+//Console.WriteLine(newArray);
